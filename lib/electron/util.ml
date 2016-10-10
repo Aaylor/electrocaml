da90f110fe366acc9343aded6c54c8910d634d84
aaylor <loic.runarvot@gmail.com>
2016-10-10 19:04:38 +0200

    util.ml: add utility functions to write methods.

--- lib/electron/util.ml
+++ lib/electron/util.ml
@@ -1,4 +1,6 @@
 
+type instance = [ `Instance  ] Js.t
+
 let electron =
   Js.Unsafe.fun_call
     (Js.Unsafe.js_expr "require")
@@ -20,11 +22,60 @@ module JSON = struct
 end
 
 module Console = struct
-
   let console = Js.Unsafe.js_expr "console"
-
   let log str =
     Js.Unsafe.meth_call
       console "log" [| Js.Unsafe.inject (Js.string str) |]
+end
 
+
+let string_param s = Js.Unsafe.inject (Js.string s)
+let int_param i = Js.Unsafe.inject i
+let float_param f = Js.Unsafe.inject (Js.float f)
+let bool_param b = Js.Unsafe.inject (Js.bool b)
+let list_param l = Js.Unsafe.inject (Js.array (Array.of_list l))
+let instance_param i = Js.Unsafe.inject i
+let callback_param cb = Js.Unsafe.(inject (meth_callback cb))
+let obj_param p = Js.Unsafe.(inject (obj p))
+let optional_param o t = match o with
+  | None -> no_param
+  | Some o -> [| t o |]
+
+module type METHODS = sig
+  val call : string -> Js.Unsafe.any array -> 'a
+  val unit_any : string -> 'a
+  val unit_unit : string -> unit
+  val unit_int : string -> int
+  val unit_bool : string -> bool
+  val unit_float : string -> float
+  val unit_string : string -> string
+  val int_unit : string -> int -> unit
+  val bool_unit : string -> bool -> unit
+  val float_unit : string -> float -> unit
+  val string_unit : string -> string -> unit
+  val callback_unit : string -> ('a -> 'b) -> unit
+  val any_unit : string -> 'a -> ('a -> Js.Unsafe.any) -> unit
+end
+
+module type INSTANCE = sig
+  val i : instance
+end
+
+module Methods (I : INSTANCE) : METHODS = struct
+  let call fname params =
+    Js.Unsafe.meth_call I.i fname params
+
+  let unit_any fname = call fname no_param
+  let unit_unit fname = unit_any fname
+  let unit_int fname = unit_any fname
+  let unit_bool fname = Js.to_bool (unit_any fname)
+  let unit_float fname = Js.to_float (unit_any fname)
+  let unit_string fname = Js.to_string (unit_any fname)
+
+  let any_unit fname p tp : unit = call fname [| tp p |]
+  let int_unit fname i = any_unit fname i int_param
+  let bool_unit fname b = any_unit fname b bool_param
+  let float_unit fname f = any_unit fname f float_param
+  let string_unit fname s = any_unit fname s string_param
+  let callback_unit fname callback = any_unit fname callback callback_param
 end
--- lib/electron/util.mli
+++ lib/electron/util.mli
@@ -1,4 +1,6 @@
 
+type instance = [ `Instance  ] Js.t
+
 val electron : 'a
 
 val __directory : unit -> string
@@ -15,3 +17,37 @@ end
 module Console : sig
   val log : string -> unit
 end
+
+val string_param : string -> Js.Unsafe.any
+val int_param : int -> Js.Unsafe.any
+val float_param : float -> Js.Unsafe.any
+val bool_param : bool -> Js.Unsafe.any
+val list_param : 'a list -> Js.Unsafe.any
+val instance_param : 'a -> Js.Unsafe.any
+val optional_param : 'a option -> ('a -> Js.Unsafe.any) -> Js.Unsafe.any array
+val obj_param : (string * Js.Unsafe.any) array -> Js.Unsafe.any
+val callback_param : ('a -> 'b) -> Js.Unsafe.any
+
+module type METHODS = sig
+  val call : string -> Js.Unsafe.any array -> 'a
+
+  val unit_any : string -> 'a
+  val unit_unit : string -> unit
+  val unit_int : string -> int
+  val unit_bool : string -> bool
+  val unit_float : string -> float
+  val unit_string : string -> string
+
+  val int_unit : string -> int -> unit
+  val bool_unit : string -> bool -> unit
+  val float_unit : string -> float -> unit
+  val string_unit : string -> string -> unit
+  val callback_unit : string -> ('a -> 'b) -> unit
+  val any_unit : string -> 'a -> ('a -> Js.Unsafe.any) -> unit
+end
+
+module type INSTANCE = sig
+  val i : instance
+end
+
+module Methods (I : INSTANCE) : METHODS
